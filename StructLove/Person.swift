d3361b23d51bd313b1a2bb6999ec82328678cc15
mariejinpark <mariejinpark@gmail.com>
2016-10-04 09:36:07 -0400

    Struct Pizza Lab

--- StructLove/Person.swift
+++ StructLove/Person.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct Person {
+    var firstName: String
+    var lastName: String
+    var home: Coordinate
+    var isHungry: Bool
+
+    init(firstName:String, lastName:String, home:Coordinate) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.home = home
+        isHungry = true
+    }
+    
+    mutating func eatPizza(from service:PizzaDeliveryService) -> Bool {
+//        if isHungry == true && service.isInRange(to: home) == true {
+//            isHungry == false
+//            return true
+//        } else if isHungry == false && !service.isInRange(to: home) == false {
+//            return false
+//        }
+//        return false
+        
+        var canEat: Bool
+        
+        if service.isInRange(to: home) {
+            if isHungry {
+                canEat = true
+                isHungry = false
+            } else {
+                canEat = false
+            }
+            
+        } else {
+            canEat = false
+        }
+        return canEat
+    }
+
+}
--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int
+    
+    init(location:Coordinate) {
+        self.location = location
+         pizzasAvailable = 10
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        if self.location.distance(to: destination) <= 5000 {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    mutating func deliverPizza(to destination:Coordinate) -> Bool {
+        
+        var canDeliver: Bool
+        
+        if isInRange(to: destination) {
+            
+            if pizzasAvailable == 0 {
+                canDeliver = false
+            } else {
+                pizzasAvailable -= 1
+                canDeliver = true
+            }
+            
+        } else {
+            canDeliver = false
+        }
+        
+        return canDeliver
+    }
+
+}
